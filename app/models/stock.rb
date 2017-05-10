ce802a2695a704cd9aa83d596c338e6c41da8697
Nabin <nabindenepali@outlook.com>
2017-05-10 10:26:01 +0545

    Create a stocks lookup form

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,10 @@
 class Stock < ActiveRecord::Base
 
+	def self.find_by_ticker(ticker_symbol)
+		where(ticker: ticker_symbol).first
+
+	end
+
 	def self.new_from_lookup(ticker_symbol)
 		looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
 		return nil unless looked_up_stock.name
@@ -19,4 +24,4 @@ class Stock < ActiveRecord::Base
 		'Unavailable' 
 	end
 
-end
+end	
